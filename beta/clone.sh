0fbf1440bbdf593becd07ea51d0b4c113261a666
kvvu <73371799+kvvu@users.noreply.github.com>
2021-05-26 13:21:31 +0700

    Update clone.sh

--- beta/clone.sh
+++ beta/clone.sh
@@ -2,5 +2,5 @@
 apt update  # To get the latest package lists
 apt install git -y && apt install python -y
 git clone https://github.com/kvvu/tweeb.git
-pip install -r requirements.txt -y
+pip install -r requirements.txt | grep -v "Requirement already satisfied"
 #
