6a534fad35482e54a33e79a4c5aa46fe1bee60cf
kvvu <73371799+kvvu@users.noreply.github.com>
2021-05-26 13:16:38 +0700

    c

--- beta/install.sh
+++ beta/install.sh
@@ -4,5 +4,5 @@ apt update  # To get the latest package lists
 apt install git -y
 apt install python -y
 git clone https://github.com/kvvu/tweeb.git
-echo "please do : python keyb.py to continue"
+echo "please go to this directory (~/tweeb/beta) and do: python keyb.py to continue"
 #
